76d47de137ee28aeb6b634d7f1d8645a94cd1eee
muchmallow <31728584+muchmallow@users.noreply.github.com>
2019-10-01 06:48:22 +0300

    Towns.js

--- src/components/Towns.js
+++ src/components/Towns.js
@@ -1,5 +1,5 @@
-export const towns = [{name: "Kharkov", id: "706483"},
+export const towns = [{name: "Kharkiv", id: "706483"},
     {name: "Kiev", id: "703448"},
     {name: "Moscow", id: "524901"},
-    {name: "Berlin", id: "7290255"},
+    {name: "Berlin", id: "2950159"},
     {name: "London", id: "2643743"}];
